c1135cc5b77ab2a6efe22c33ca039a5815bcb3f2
Tim @katok <katkov0103@yandex.ru>
2023-10-07 11:58:04 +0300

    Added lots of new tables to project

--- src/main/java/ru/katok/tamctf/entity/Task.java
+++ src/main/java/ru/katok/tamctf/entity/Task.java
@@ -10,9 +10,8 @@ import jakarta.persistence.Id;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
     private Long id;
-
 
     private String name;
     private String description;
@@ -39,5 +38,77 @@ public class Task {
 
     public String toString() {
         return String.format("<Task id=%d name=%s>", id, name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAdminDescription() {
+        return adminDescription;
+    }
+
+    public void setAdminDescription(String adminDescription) {
+        this.adminDescription = adminDescription;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public double getScoreInitial() {
+        return scoreInitial;
+    }
+
+    public void setScoreInitial(double scoreInitial) {
+        this.scoreInitial = scoreInitial;
+    }
+
+    public double getScoreDelay() {
+        return scoreDelay;
+    }
+
+    public void setScoreDelay(double scoreDelay) {
+        this.scoreDelay = scoreDelay;
+    }
+
+    public double getScoreMinimum() {
+        return scoreMinimum;
+    }
+
+    public void setScoreMinimum(double scoreMinimum) {
+        this.scoreMinimum = scoreMinimum;
     }
 }
