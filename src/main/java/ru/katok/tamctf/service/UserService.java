82c0ade1a5e9c5c5fd49f1a3ca6a41a2b321794c
Tim @katok <katkov0103@yandex.ru>
2023-10-13 23:57:17 +0300

    not added userservice in previous. bruh

--- src/main/java/ru/katok/tamctf/service/UserService.java
+++ src/main/java/ru/katok/tamctf/service/UserService.java
@@ -14,53 +14,10 @@ import ru.katok.tamctf.repository.UserRepository;
 public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
-//    @Autowired
-//    RoleRepository roleRepository;
-//
-//    @Autowired
-//    PasswordEncoder passwordEncoder;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return UserDto.fromApplicationUser(
                 userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found!"))
         );
     }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        return UserDetailsImpl.fromApplicationUser(
-//                repository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found!"))
-//        );
-//    }
-//
-//    public User findUserById(Long userId) {
-//        Optional<User> userFromDb = userRepository.findById(userId);
-//        return userFromDb.orElse(new User());
-//    }
-//
-//    public List<User> allUsers() {
-//        return userRepository.findAll();
-//    }
-//
-//    public boolean saveUser(User user) {
-//        User userFromDB = userRepository.findByUsername(user.getUsername());
-//
-//        if (userFromDB != null) {
-//            return false;
-//        }
-//
-//        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
-//        userRepository.save(user);
-//        return true;
-//    }
-//
-//    public boolean deleteUser(Long userId) {
-//        if (userRepository.findById(userId).isPresent()) {
-//            userRepository.deleteById(userId);
-//            return true;
-//        }
-//        return false;
-//    }
 }
