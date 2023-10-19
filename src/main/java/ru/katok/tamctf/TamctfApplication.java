2cc26d197b8253d710e8d17708d104c67099afe8
DrunkardKirA <fkesh10@gmail.com>
2023-10-19 16:41:34 +0300

    added start index + css, merge conflict resolve

--- src/main/java/ru/katok/tamctf/TamctfApplication.java
+++ src/main/java/ru/katok/tamctf/TamctfApplication.java
@@ -1,11 +1,9 @@
 package ru.katok.tamctf;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
