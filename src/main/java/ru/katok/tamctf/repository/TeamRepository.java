488944f30ef8a01aba9079d58897b3c03865027d
Tim @katok <katkov0103@yandex.ru>
2023-10-23 12:09:07 +0300

    Added basic repos

---
+++ src/main/java/ru/katok/tamctf/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package ru.katok.tamctf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.katok.tamctf.domain.entity.Team;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
