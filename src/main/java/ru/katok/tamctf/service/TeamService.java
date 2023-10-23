098ff761d0a3fe03546b1cde008b834a96238a36
Tim @katok <katkov0103@yandex.ru>
2023-10-23 12:24:26 +0300

    Added basic team controller for admin

--- src/main/java/ru/katok/tamctf/service/TeamService.java
+++ src/main/java/ru/katok/tamctf/service/TeamService.java
@@ -1,6 +1,7 @@
 package ru.katok.tamctf.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.katok.tamctf.domain.entity.Team;
 import ru.katok.tamctf.repository.TeamRepository;
 import ru.katok.tamctf.service.interfaces.ITeamService;
@@ -8,6 +9,7 @@ import ru.katok.tamctf.service.interfaces.ITeamService;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Service("teamService")
 public class TeamService implements ITeamService {
     private final TeamRepository teamRepository;
 
